d22daf04fb77e85044cc264e5a157f0237454101
YaeshwanthUrumaiya <yaeshwanthurumaiya@outlook.com>
2023-04-01 23:39:59 +0530

    Plotly graphs added

--- Test_Flask/Test_Flask.py
+++ Test_Flask/Test_Flask.py
@@ -46,13 +46,23 @@ def model_prediction(df):
 
 #the prediction for the model rn is now in scaled model. so you are rescaling it backwards
     predictions = scaler.inverse_transform(predictions)
-    plt.plot(Y_val)
-    plt.plot(predictions)
+    #plt.plot(Y_val)
+    #plt.plot(predictions)
 
     # Save the chart to a temporary file
-    chart_file = "Stock-Price-Prediction-Model\Test_Flask\static\Image\output-1.jpg"
-    plt.savefig(chart_file)
-    return str(predictions[-1][0]),predictions
+    #chart_file = "Stock-Price-Prediction-Model\Test_Flask\static\Image\output-1.jpg"
+    #plt.savefig(chart_file)
+    trace1 = go.Scatter(x=df.index, y=Y_val.reshape(-1), name='Actual')
+    trace2 = go.Scatter(x=df.index, y=predictions.reshape(-1), name='Predicted')
+
+    # Combine traces into data and create layout
+    data = [trace1, trace2]
+    layout = go.Layout(title='Actual vs Predicted', xaxis_title='Date', yaxis_title='Price')
+
+    # Create figure and plot
+    fig = go.Figure(data=data, layout=layout)
+    graph_html = pyo.plot(fig, output_type="div")
+    return str(predictions[-1][0]),graph_html
 
 
 
@@ -68,13 +78,9 @@ def gfg():
        stockname=request.form.get('sname')
 
        df=yk.download(tickers=stockname,period='60d',interval='30m')
-       va,ds=model_prediction(df)
-       return render_template('index1.html', my_string=va,predictions=ds)
-    data = [go.Scatter(x=[1, 2, 3], y=[4, 5, 6])]
-    layout = go.Layout(title="My Plot")
-    fig = go.Figure(data=data, layout=layout)
-    graph_html = pyo.plot(fig, output_type="div")
-    return render_template("index1.html", graph_html=graph_html)  
+       va,graph_html=model_prediction(df)
+       return render_template('index1.html', my_string=va,graph_html=graph_html)
+    return render_template("index1.html")  
 
   
 if __name__ =="__main__":  
