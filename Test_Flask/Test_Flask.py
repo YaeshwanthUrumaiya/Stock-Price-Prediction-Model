a65382de8620e3e31f9ce7aeb80d247acee595be
YaeshwanthUrumaiya <yaeshwanthurumaiya@outlook.com>
2023-04-02 17:52:13 +0530

    Ver 1

--- Test_Flask/Test_Flask.py
+++ Test_Flask/Test_Flask.py
@@ -1,17 +1,14 @@
 from flask import *
 import numpy as np 
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import yfinance as yk
-# those are the basics. 
+
 
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler,MinMaxScaler
-from sklearn.metrics import r2_score
-#to get the test train data split and then scale them. 
+from sklearn.preprocessing import MinMaxScaler
+ 
 
-from keras.models import Sequential #sequential is a type of neutral network where the bottom layer feeds the layer infront. going in a sequential order
+from keras.models import Sequential
 from keras.layers import Dense, LSTM, Dropout
 
 import plotly.graph_objs as go
@@ -25,10 +22,8 @@ def model_prediction(df):
 
     X=X.values
     Y=Y.values
-    X_train, X_val, Y_train, Y_val = train_test_split(X,Y,test_size=0.05, random_state=10)
 
-    Y_train_data=pd.DataFrame(Y_train[:,1])
-    Y_val_data=pd.DataFrame(Y_val[:,1])
+    X_train, X_val, Y_train, Y_val = train_test_split(X,Y,test_size=0.05, random_state=10)
 
     Y_train=np.delete(Y_train,1,axis=1)
     Y_val=np.delete(Y_val,1,axis=1)
@@ -36,81 +31,55 @@ def model_prediction(df):
 
     scaler = MinMaxScaler(feature_range=(0,1))
     Y_train=scaler.fit_transform(Y_train)
-    y_val=scaler.fit(Y_val)
-#that is the bottom layer and it goes further and further. 
-#return sequences being true means all of the hidden weights and baises will move to the next layer. 
-#if it is set to false, then it won't and only the true output from that layer will move to the next layer. 
-    model = Sequential()
-#that is the bottom layer and it goes further and further. 
-#return sequences being true means all of the hidden weights and baises will move to the next layer. 
-#if it is set to false, then it won't and only the true output from that layer will move to the next layer. 
-    model.add(LSTM(169, return_sequences=True, input_shape= (X_train.shape[1], 1)))
-    model.add(Dropout(0.2))
-    model.add(LSTM(84, return_sequences=True))
-    model.add(Dropout(0.2))
-    model.add(LSTM(64, return_sequences=False))
-    model.add(Dropout(0.2))
-    model.add(Dense(40))
-    model.add(Dense(1)) 
-    model.compile(optimizer='adam', loss='mean_squared_error')
-#optimizer is how you train the data. usually you would use gradient descent to update the parameters. 
-#adam is another option which is pretty much gradient descent based on the loss function but it's a bit better and works a bit differnetly. 
-#and the loss is the function. 
 
-# Train the model
-    model.fit(X_train, Y_train, batch_size=5, epochs=5,shuffle=True)
-    #so you have a trained model now. so you are now testing. 
-    predictions = model.predict(X_val)
+    model1 = Sequential()
+ 
+    model1.add(LSTM(169, return_sequences=True, input_shape= (X_train.shape[1], 1)))
+    model1.add(Dropout(0.2))
+    model1.add(LSTM(84, return_sequences=True))
+    model1.add(Dropout(0.2))
+    model1.add(LSTM(64, return_sequences=False))
+    model1.add(Dropout(0.2))
+    model1.add(Dense(40))
+    model1.add(Dense(1)) 
 
-#the prediction for the model rn is now in scaled model. so you are rescaling it backwards
+    model1.compile(optimizer='adam', loss='mean_absolute_error')
+
+    model1.fit(X_train, Y_train, batch_size=5, epochs=5,shuffle=True)
+
+    predictions = model1.predict(X_val)
+
     predictions = scaler.inverse_transform(predictions)
-
-    r2s=r2_score(Y_val, predictions)
-#that is the r2 score. mulitplication of that value with 100 will give you the percentage value of accuracy. the closer this is to 1 is better. 
     rsme=np.sqrt(np.mean(((predictions - Y_val) ** 2)))
+
     todays_data=df.iloc[-1]
     todays_data.drop(['Close'],inplace=True)
     todays_data=np.array([todays_data])
-    data_rn=model.predict(todays_data)
+    data_rn=model1.predict(todays_data)
     todays_value=scaler.inverse_transform(data_rn)
-    #plt.plot(Y_val)
-    #plt.plot(predictions)
 
-    # Save the chart to a temporary file
-    #chart_file = "Stock-Price-Prediction-Model\Test_Flask\static\Image\output-1.jpg"
-    #plt.savefig(chart_file)
-    #train_dates = df.index[:-len(X_val)]
-    val_dates = df.index[-len(X_val):]
+    t1 = go.Scatter(x = list(range(len(Y_val[:,0]))), y=Y_val[:,0], name="Actual Close Price of Testing Data")
+    t2 = go.Scatter(x = list(range(len(Y_val[:,0]))), y=predictions[:,0], name="Predicted Close Price of Testing Data")
 
-    # plot the time series graph of the close price of the training data
-    #trace1 = go.Scatter(x=train_dates, y=scaler.inverse_transform(Y_train)[:,0], mode="lines", name="Close Price of Training Data")
-
-    # plot the actual close price of the testing data
-    trace2 = go.Scatter(x = list(range(len(Y_val[:,0]))), y=Y_val[:,0], name="Actual Close Price of Testing Data")
-
-    # plot the predicted price of the testing data
-    trace3 = go.Scatter(x = list(range(len(Y_val[:,0]))), y=predictions[:,0], name="Predicted Close Price of Testing Data")
-
-    #data = [trace1, trace2, trace3]
-    data = [trace2, trace3]
+    data = [t1, t2]
     layout = dict(title='Stock Price Prediction', xaxis_title='Date', yaxis_title='Price',margin=dict(l=100, r=100, t=200, b=200),plot_bgcolor="rgba(0,0,0,0)",paper_bgcolor="rgba(0,0,0,0)") 
     fig = dict(data=data, layout=layout)
 
-    graph_html = pyo.plot(fig, output_type="div")
+    graph1 = pyo.plot(fig, output_type="div")
     
-    strout= "The predicted value is:"+str(todays_value[0][0])+" with accurary of:"+str((r2s*100))+" and RSME of:"+str(rsme)
     s1=str(round(todays_value[0][0], 2))
     s2=str(np.ceil(rsme))
 
-    return s1,graph_html,s2
+    return s1,graph1,s2
 
 
 
 app = Flask(__name__)  
  
 @app.route('/', methods =["GET", "POST"])  
-def gfg():  
+def test():  
     if request.method == "POST":
+
         stockname=request.form.get('sname')
         per=request.form.get('per')
 
@@ -135,26 +104,15 @@ def gfg():
             df=yk.download(tickers=stockname,period=per,interval=inter)
         except:
             return render_template("index.html", error="We are unable to fetch the data")
-        #go with either 60d and 30m or 3y and 1d or 60d and 60m or 90d and 60m
+
         if (((np.array(df.sum()))!=0).sum()==0):
             return render_template("index.html", error="We are unable to fetch the data")
-        s1,graph_html,s2=model_prediction(df)
-        return render_template('graph.html',my_string1=s1,my_string2=s2,graph_html=graph_html)
+        
+        s1,graph1,s2=model_prediction(df)
+
+        return render_template('graph.html',string1=s1,string2=s2,graph1=graph1)
     
     return render_template("index.html")  
 
-    #return render_template("graph.html")
-
-    #trace = go.Scatter(x=[1, 2, 3], y=[4, 5, 6])
-
-# create the layout and set the margin
-    #layout = go.Layout(title='My Plot',xaxis=dict(title='X Axis'),yaxis=dict(title='Y Axis'),margin=dict(l=100, r=100, t=200, b=200)  # set the margins to 50 pixels)
-    #fig = dict(data=trace, layout=layout)
-    #graph_html = pyo.plot(fig, output_type="div")
-    #return render_template('graph.html',graph_html=graph_html)
-
-# create the figure and plot it
-
-  
 if __name__ =="__main__":  
     app.run(debug = True)  
